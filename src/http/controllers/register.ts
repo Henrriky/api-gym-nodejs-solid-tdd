54709f825a0343a31e20641b665b3f7cf2e3bb8b
Henrriky <tectutors.123@gmail.com>
2023-11-23 23:25:00 -0300

    refactor: local and global error handling

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -4,17 +4,16 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { hash } from 'bcrypt'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
-    name: z.string(),
+    name: z.string(), 
     email: z.string().email(),
     password: z.string().min(6),
   })
   const { name, email, password } = registerBodySchema.parse(request.body)
-
-  const password_hash = await hash(password, 6)
 
   try {
     const prismaRepository = new PrismaUsersRepository()
@@ -25,7 +24,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password
     })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message})
+    }
+    return reply.status(500).send() // TODO: fix me
   }
 
   return reply.status(201).send()
