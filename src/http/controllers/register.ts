71828612edf6b10031c7f342d2a70d76600abd49
Henrriky <tectutors.123@gmail.com>
2023-11-25 17:34:56 -0300

    refactor: throw error to global handler and solving imports

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -26,7 +26,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message})
     }
-    return reply.status(500).send() // TODO: fix me
+    throw error
   }
 
   return reply.status(201).send()
