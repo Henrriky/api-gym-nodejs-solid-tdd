74ee7778460680dcfd88c7c01a00366e741450de
Henrriky <tectutors.123@gmail.com>
2023-11-26 18:42:34 -0300

    test: add unit tests for search gyms paginated use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,5 @@
 import { Gym, Prisma } from "@prisma/client";
 import { GymsRepository } from "../gyms-repository";
-import { Decimal } from "@prisma/client/runtime/library";
 import { randomUUID } from "node:crypto";
 
 export class InMemoryGymsRepository implements GymsRepository {
@@ -16,15 +15,25 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym
   }
 
-  async create (data: Prisma.GymCreateInput) {
-    
+  async searchMany(query: string, page: number) {
+
+    const indexStart = (page - 1) * 20
+    const indexEnd = (page * 20)
+
+    return this.itens
+      .filter((item) => item.title.includes(query))
+      .slice(indexStart, indexEnd)
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+
     const gym: Gym = {
       id: data.id ?? randomUUID(),
       title: data.title,
       description: data.description ?? null,
       phone: data.phone ?? null,
       latitude: new Prisma.Decimal(data.latitude.toString()),
-      longitude: new Prisma.Decimal(data.longitude.toString()),      
+      longitude: new Prisma.Decimal(data.longitude.toString()),
       created_at: new Date()
     }
 
