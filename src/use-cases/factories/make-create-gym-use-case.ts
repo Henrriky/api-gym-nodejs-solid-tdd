7943c1bd3a9d86392ab4d1c96ff0980fe535923f
Henrriky <tectutors.123@gmail.com>
2023-12-03 18:19:41 -0300

    feat: add user use cases controller

--- src/use-cases/factories/make-create-gym-use-case.ts
+++ src/use-cases/factories/make-create-gym-use-case.ts
@@ -1,7 +1,7 @@
 import { CreateGymUseCase } from "../create-gym"
 import { PrismaGymsRepository } from "@/repositories/prisma/prisma-gyms-repository"
 
-export function makeFetchNearbyGymsUseCase() {
+export function makeCreateGymUseCase() {
   const gymsRepository = new PrismaGymsRepository()
   const usecase = new CreateGymUseCase(gymsRepository)
 
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,10 +1,6 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
-import { GymsRepository } from "@/repositories/gyms-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
-import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-two-coordinates";
-import { MaxDistanceError } from "./errors/max-distance-error";
-import { MaxNumbersOfCheckInsError } from "./errors/max-numbers-of-check-ins-error";
 import dayjs from "dayjs";
 import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
